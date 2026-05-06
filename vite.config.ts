6202bcc262485fb43629ee5c64d714eaa99a700e
creed <admin@cyfee.com>
2026-05-06 10:59:59 +0800

    fix(dev): add proxy for /media to accommodate relative avatar paths

--- vite.config.ts
+++ vite.config.ts
@@ -69,6 +69,10 @@ export default defineConfig(({ mode }) => {
           target: 'ws://127.0.0.1:8000',
           ws: true,
           changeOrigin: true
+        },
+        '/media': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
         }
       }
     }
